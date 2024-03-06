c30fdc47196f52cd3802464c99313bdd4ff31c89
Ellis Walmsley <68521136+EllisWeb@users.noreply.github.com>
2024-03-06 09:38:54 +0000

    Installing required packages and outlining program.
    Installed prompt-sync package to gather user input.
    
    Outlined project goals to create sub-tasks and allow me to prioritise them.

---
+++ project.js
@@ -0,0 +1,7 @@
+// 1. Deposit money
+// 2. Determine number of lines to bet on
+// 3. Collect bet amount
+// 4. Spin the slot machine
+// 5. Determine if the user won
+// 6. Give the user their winnings or take the bet
+// 7. Play again or handle if the user has no money left
